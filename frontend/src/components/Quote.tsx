17d621ed5c9315ba66a92e6a046f53202515a188
sanjayramabuddi <sanjayramabuddi@gmail.com>
2025-06-23 23:51:53 +0530

    frontend pages added

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,14 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex flex-col justify-center items-center">
+      <div className="max-w-xl">
+        <div className="text-2xl font-medium">
+          "The customer service I received was exceptional. The support team
+          went above and beyond to address my concerns"
+        </div>
+        <div className="text-xl mt-2">Jules Winfield</div>
+        <div className="text-sm text-slate-500">CEO, Acme Inc</div>
+      </div>
+    </div>
+  );
+};
